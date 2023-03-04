28ba2afcf7b03afe4a16b8b8ca9feae838ccbbdf
Istiak Ahmed <istiak.inam@gmail.com>
2023-03-04 11:34:05 +0600

    added POST

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/help', (req, res) => {
-    res.send('Hello World');
+app.get('/', (req, res) => {
+    res.send(`
+        <div>
+            <form method='POST'>
+                <input name='email' placeholder='email'/>
+                <input name='password' placeholder='password'/>
+                <input name='passwordConfirmation" placeholder='re-enter password'/>
+                <button>Submit</button>
+            </form>
+        </div>
+    `);
+})
+
+app.post('/', (req, res) => {
+    res.send("Account Created");
 })
 
 app.listen(port, () => {
