ac6bfd43ca0205b2bb760b65004832b5f3961391
Istiak Ahmed <istiak.inam@gmail.com>
2023-03-04 10:06:02 +0600

    added express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/help', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(port, () => {
+    console.log('Example with express')
+})
