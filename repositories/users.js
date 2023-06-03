4af586d31e6524702ed74aa9f5abec9ee660d2c1
Istiak Ahmed <istiak.inam@gmail.com>
2023-06-03 17:16:32 +0600

    added hashed and salted and compared

--- repositories/users.js
+++ repositories/users.js
@@ -1,6 +1,9 @@
 import fs from 'fs';
 import { get } from 'http';
 import crypto from 'crypto'
+import util from 'util'
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
     constructor(filename) {
@@ -28,13 +31,22 @@ class UsersRepository {
     async create(attrs) {
         attrs.id = this.randomId();
 
+        const randomBytes = crypto.randomBytes(8);
+        const salt = randomBytes.toString('hex');
+        const buf = await scrypt(attrs.password, salt, 64)
+
         const records = await this.getAll()
         
-        //adding the new user
-        records.push(attrs)
+        //adding the new user with hashed pass into the db
+        const record = ({
+            ...attrs,
+            password: `${buf.toString('hex')}.${salt}`
+        })
+
+        records.push(record)
 
         await this.writeAll(records)
-        return attrs; 
+        return record; 
     }
 
     async writeAll(records) {
