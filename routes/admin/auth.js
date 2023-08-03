074442f6d1b67fe92a8f7c929923f6a3eda0415d
istiakinam <istiak.inam@gmail.com>
2023-08-03 20:43:05 +0600

    added middlewares.js

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,7 @@
-import usersRepo from '../../repositories/users.js';
 import express from 'express'
+
+import { handleErrors } from './middlewares.js';
+import usersRepo from '../../repositories/users.js';
 import { check, validationResult } from 'express-validator'
 import signupTemplate from '../../views/admin/auth/signup.js'
 import signinTemplate from '../../views/admin/auth/signin.js'
@@ -16,19 +18,12 @@ router.get('/signup', (req, res) /*middleware function*/ => {
     res.send(signupTemplate({ req }));
 })
 
-router.post('/signup', 
-    [
-        requireEmail,
-        requirePassword,
-        requirePasswordConfirmation
-    ],
+router.post(
+    '/signup', 
+    [requireEmail, requirePassword, requirePasswordConfirmation],
+    handleErrors(signupTemplate),
     async (req, res) => {
-        const errors = validationResult(req); //all the validation results from above is attached to the req object
-        if(!errors.isEmpty()) {
-            return res.send(signupTemplate({ req, errors }))
-        }
-
-        const { email, password, passwordConfirmation } = req.body;
+        const { email, password } = req.body;
 
         //create user inside user repo
         const user = await usersRepo.create({ email, password });
@@ -53,13 +48,8 @@ router.post('/signin',
         checkEmail,
         checkPassword        
     ], 
+    handleErrors(signinTemplate),
     async (req, res) => {
-        const errors = validationResult(req)
-        
-        if(!errors.isEmpty()) {
-            return res.send(signinTemplate({ errors }))
-        }
-
         const { email } = req.body;
         const user = await usersRepo.getOneBy({ email })    
 
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 import express from 'express'
-import { validationResult } from 'express-validator'
 import multer from 'multer'
 
+import { handleErrors } from './middlewares.js'
 import productsRepo from '../../repositories/products.js'
 import productsNewTemplate from '../../views/admin/products/new.js'
 import { requireTitle, requirePrice } from './validators.js'
@@ -15,14 +15,17 @@ router.get('/admin/products/new', (req, res) => {
     res.send(productsNewTemplate({}))
 })
 
-router.post('/admin/products/new', [requireTitle, requirePrice], upload.single('image'), async (req, res) => {
-    const errors = validationResult(req)
+router.post(                    //ordering of middleware is important
+    '/admin/products/new',  
+    upload.single('image'),
+    [requireTitle, requirePrice], 
+    handleErrors(productsNewTemplate),
+    async (req, res) => {
+        const image = req.file.buffer.toString('base64')
+        const { title, price } = req.body
+        await productsRepo.create({ title, price, image })
 
-    const image = req.file.buffer.toString('base64')
-    const { title, price } = req.body
-    await productsRepo.create({ title, price, image })
-
-    res.send('Submitted')   
+        res.send('Submitted')   
 })
 
 export default router
