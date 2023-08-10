0ee17224de241b716c1117caa132e54a155d3faf
istiakinam <istiak.inam@gmail.com>
2023-08-10 23:01:55 +0600

    add to cart route

--- routes/carts.js
+++ routes/carts.js
@@ -3,6 +3,10 @@ import express from 'express'
 const router = express.Router();
 
 //Receive a POST request to add an item to the cart
+router.post('/cart/products', (req, res) => {
+    console.log(req.body.productId)
+    res.send(req.body.productId)
+})
 
 //Receive a GET request to display all items in the cart
 
